4904725792746073ed4f85532689f97e0072478f
rodolfostark <rodolfojonatas@outlook.com>
2024-05-05 11:46:05 -0300

    chore: code up solution for problem 6

---
+++ DataFormat/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DateFormat;
+
+public enum DataFormats
+{
+    Completo = 0,
+    ApenasData = 1,
+    ApenasHora = 2,
+    PorExtenso = 3,
+}
+
+class DateFormat
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o formato de exibição da data atual: ");
+        Console.WriteLine("0 - Formato completo (dia da semana, dia do mês, mês, ano, hora, minutos, segundos).");
+        Console.WriteLine("1 - Apenas a data no formato \"01/03/2024\"");
+        Console.WriteLine("2 - Apenas a hora no formato de 24 horas.");
+        Console.WriteLine("3 - A data com o mês por extenso.");
+
+        var dateFormat = Convert.ToInt32(Console.ReadLine());
+        switch (dateFormat) 
+        {
+            case (int)DataFormats.Completo:
+                Console.WriteLine(DateTime.Now.ToString());
+                break;
+            case (int)DataFormats.ApenasData:
+                Console.WriteLine(DateTime.Now.Date.ToString());
+                break;
+            case (int)DataFormats.ApenasHora:
+                Console.WriteLine(DateTime.Now.Hour.ToString());
+                break;
+            case (int)DataFormats.PorExtenso:
+                Console.WriteLine(DateTime.Now.ToShortDateString());
+                break;
+            default:
+                Console.WriteLine("Formato de data informado é inválido");
+                break;
+
+            
+        }
+        
+    }
+}
